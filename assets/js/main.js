1393e1ee5fe3b0859406ce50adf8c3be7c9ade0b
Volker Otto <volker.otto@digital-masters.de>
2023-05-11 01:53:28 +0200

    💄 fixed some more css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,21 @@
+"use strict";
 var dmToggle = document.getElementById("darkmode-toggle");
 var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
 var currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
     document.body.classList.toggle("dark");
-} else if (currentTheme == "light") {
+}
+else if (currentTheme == "light") {
     document.body.classList.toggle("light");
 }
-
 dmToggle.addEventListener("click", function () {
     if (prefersDarkScheme.matches) {
         document.body.classList.toggle("light");
         var theme = document.body.classList.contains("light")
             ? "light"
             : "dark";
-    } else {
+    }
+    else {
         document.body.classList.toggle("dark");
         var theme = document.body.classList.contains("dark") ? "dark" : "light";
     }
