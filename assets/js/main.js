1082682d80625847c251a6ec558c2ee6abe35c6a
Volker Otto <volker.otto@digital-masters.de>
2023-05-11 23:45:25 +0200

    💄fix darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
 "use strict";
-var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 var currentTheme = localStorage.getItem("theme");
+
 if (currentTheme == "dark") {
-  document.body.classList.toggle("dark");
+  document.body.classList.add("dark");
 }
-else if (currentTheme == "light") {
-  document.body.classList.toggle("light");
-}
+
 function toggleDarkMode() {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light");
-    var theme = document.body.classList.contains("light") ? "light" : "dark";
-  }
-  else {
-    document.body.classList.toggle("dark");
-    var theme = document.body.classList.contains("dark") ? "dark" : "light";
-  }
+
+  document.body.classList.toggle("dark");
+  var theme = document.body.classList.contains("dark") ? "dark" : "light";
+
   localStorage.setItem("theme", theme);
   return false;
 }
+
