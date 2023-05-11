3c5c5a92c8fb1f516dc2ffbeb32b47155e1ee582
Volker Otto <volker.otto@digital-masters.de>
2023-05-11 23:17:17 +0200

    more dark mode changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,21 @@
 "use strict";
-var dmToggle = document.getElementById("darkmode-toggle");
 var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 var currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
-    document.body.classList.toggle("dark");
+  document.body.classList.toggle("dark");
 }
 else if (currentTheme == "light") {
-    document.body.classList.toggle("light");
+  document.body.classList.toggle("light");
 }
-dmToggle.addEventListener("click", function () {
-    if (prefersDarkScheme.matches) {
-        document.body.classList.toggle("light");
-        var theme = document.body.classList.contains("light")
-            ? "light"
-            : "dark";
-    }
-    else {
-        document.body.classList.toggle("dark");
-        var theme = document.body.classList.contains("dark") ? "dark" : "light";
-    }
-    localStorage.setItem("theme", theme);
-});
+function toggleDarkMode() {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light");
+    var theme = document.body.classList.contains("light") ? "light" : "dark";
+  }
+  else {
+    document.body.classList.toggle("dark");
+    var theme = document.body.classList.contains("dark") ? "dark" : "light";
+  }
+  localStorage.setItem("theme", theme);
+  return false;
+}
